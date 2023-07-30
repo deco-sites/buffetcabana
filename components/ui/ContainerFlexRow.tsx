74ebd9b2491a978bb6f139d41102208528a05ad7
Gabriel Almeida <gabriel@agenciaeplus.com.br>
2023-07-30 16:42:31 -0300

    Bloco de container para sections

---
+++ components/ui/ContainerFlexRow.tsx
@@ -0,0 +1,16 @@
+import { isSection, Section } from "$live/blocks/section.ts";
+
+export interface Props {
+  leftContent: Section;
+  rightContent: Section;
+  className?: string;
+}
+
+export default function ContainerFlexRow({ leftContent, rightContent, className }: Props) {
+  return (
+    <div class={`flex md:flex-row ${className && className}`}>
+      {leftContent}
+      {rightContent}
+    </div>
+  );
+}
